a5420cc27fef35880c7c5db1fa2b8abae3fba0dc
Ekramul Haque <ehr.ekramul@gmail.com>
2025-06-19 23:10:09 +0600

    Refactor Structure

--- DigitalArena/Controllers/Admin/ManageBuyerController.cs
+++ DigitalArena/Controllers/Admin/ManageBuyerController.cs
@@ -2,6 +2,7 @@
 using DigitalArena.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
---
+++ DigitalArena/Scripts/custom/Password_Visibility_Icon.js
@@ -0,0 +1,13 @@
+﻿function togglePasswordVisibility() {
+    var input = document.getElementById("passwordInput");
+    var icon = document.getElementById("eyeIcon");
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("bi-eye");
+        icon.classList.add("bi-eye-slash");
+    } else {
+        input.type = "password";
+        icon.classList.remove("bi-eye-slash");
+        icon.classList.add("bi-eye");
+    }
+}
